c49d273733b2c408b63b6edda76dfb19d7a57e4d
mathisarends <ma197404@fh-muenster.de>
2024-07-08 16:06:22 +0200

    Setup folder structure | angular frontend | express backend

---
+++ server/src/app.ts
@@ -0,0 +1,29 @@
+import express, { Express } from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import http from 'node:http';
+
+import config from '../config.json' assert { type: 'json' };
+
+function configureApp(app: Express) {
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use(cookieParser());
+  app.use(cors());
+}
+
+export async function start() {
+  const app = express();
+  configureApp(app);
+  startHttpServer(app, config.server.port);
+}
+
+async function startHttpServer(app: Express, port: number) {
+  const httpServer = http.createServer(app);
+  httpServer.listen(port, () => {
+    app.locals.port = port;
+    console.log(`Server running at http://localhost:${port}`);
+  });
+}
+
+start();
