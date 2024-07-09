0e29f45c7427f45227bede7829264d66a2c73911
mathisarends <ma197404@fh-muenster.de>
2024-07-09 07:25:11 +0200

    Implement router represent link active states

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -8,4 +9,48 @@ import { RouterModule } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  @ViewChildren('navLink') navLinks!: QueryList<ElementRef>;
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.updateActiveLink();
+      });
+  }
+
+  async navigateTo(event: Event) {
+    event.preventDefault();
+
+    const linkElement = event.target as HTMLAnchorElement;
+    const url = new URL(linkElement.href).pathname;
+
+    this.removeActiveState();
+    linkElement.classList.add('active');
+
+    try {
+      await this.router.navigate([url]);
+    } catch (error) {
+      console.error('Navigation error:', error);
+    }
+  }
+
+  removeActiveState() {
+    this.navLinks.forEach((link) => {
+      link.nativeElement.classList.remove('active');
+    });
+  }
+
+  updateActiveLink() {
+    this.removeActiveState();
+    const currentUrl = this.router.url;
+
+    this.navLinks.forEach((link) => {
+      const linkElement = link.nativeElement as HTMLAnchorElement;
+      if (linkElement.pathname === currentUrl) {
+        linkElement.classList.add('active');
+      }
+    });
+  }
+}
