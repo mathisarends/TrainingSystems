d7b6531938167c454f87f796c4eccc021fe0cf46
mathisarends <ma197404@fh-muenster.de>
2024-07-09 10:22:01 +0200

    Parametrized hero section

--- client/src/app/Pages/landing/landing.component.ts
+++ client/src/app/Pages/landing/landing.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { Slide } from '../../types/slide';
 
 import { CarouselComponent } from '../../components/carousel/carousel.component';
+import { HeroComponent } from '../../hero/hero.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [CarouselComponent],
+  imports: [CarouselComponent, HeroComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss',
 })
@@ -31,4 +32,13 @@ export class LandingComponent {
       text: 'Some representative placeholder content for the third slide.',
     },
   ];
+
+  heroHeadline = 'Training Systems';
+  heroLeadTexts = [
+    'Track Your Powerlifting Progress and Connect with the Community!',
+    'Join Training Systems to record your powerlifting journey, set personal records, and share your achievements with fellow lifters.',
+    "Whether you're a beginner or an experienced lifter, our app provides a platform to inspire, support, and learn from each other's successes.",
+    'Our advanced tracking system ensures you never miss a workout, offering tailored plans and progress reports. Engage with a community of like-minded lifters, join challenges, and find inspiration from others’ journeys. With Training Systems, every lift counts!',
+  ];
+  heroButtonText = 'Get Started';
 }
