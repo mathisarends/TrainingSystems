728dab998186e876fc8a53fe389fb26d1d41df92
mathisarends <ma197404@fh-muenster.de>
2024-07-10 08:17:52 +0200

    Add generic dao for user fix login layout

---
+++ server/src/models/user.ts
@@ -0,0 +1,25 @@
+/* Autor: Mathis Kristoffer Arends */
+import { Entity } from './entity.js';
+
+/**
+ * @interface User
+ * Represents a user with authentication details.
+ *
+ * @extends Entity
+ */
+export interface User extends Entity {
+  /**
+   * The username of the user.
+   */
+  username: string;
+
+  /**
+   * The email address of the user.
+   */
+  email: string;
+
+  /**
+   * The password of the user.
+   */
+  password: string;
+}
