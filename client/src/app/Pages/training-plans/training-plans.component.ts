b4afc66747bcc743a4dfc40bdae7c90bcb268436
mathisarends <ma197404@fh-muenster.de>
2024-07-11 17:00:55 +0200

    Implement delete confirmation via modalservice

--- client/src/app/Pages/training-plans/training-plans.component.ts
+++ client/src/app/Pages/training-plans/training-plans.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalService } from '../../../service/modalService';
 import { CreateTrainingFormComponent } from '../../create-training-form/create-training-form.component';
+import { DeleteConfirmationComponent } from '../../delete-confirmation/delete-confirmation.component';
 import { HttpClientService } from '../../../service/http-client.service';
 import { HttpMethods } from '../../types/httpMethods';
 
@@ -11,6 +12,7 @@ import { TrainingCardsComponent } from '../../components/training-card/training-
 
 import { BasicTrainingPlanView } from '../../../../../shared/models/dtos/training/trainingDto.types.js';
 import { HttpErrorResponse } from '@angular/common/http';
+import { ModalEventsService } from '../../../service/modal-events.service';
 
 @Component({
   selector: 'app-training-plans',
@@ -23,9 +25,12 @@ export class TrainingPlansComponent implements OnInit {
   protected trainingPlans!: BasicTrainingPlanView[];
   protected isLoading: boolean = true;
 
+  private deleteIndex: number = -1;
+
   constructor(
     private modalService: ModalService,
-    private httpClient: HttpClientService
+    private httpClient: HttpClientService,
+    private modalEventsService: ModalEventsService
   ) {}
 
   ngOnInit(): void {
@@ -35,6 +40,12 @@ export class TrainingPlansComponent implements OnInit {
         this.trainingPlans = response.trainingPlanDtos;
         this.isLoading = false;
       });
+
+    // Listen for confirm event
+    this.modalEventsService.confirmClick$.subscribe(() => {
+      console.log('ist was passiert');
+      this.handleDelete(this.deleteIndex);
+    });
   }
 
   createNewPlan() {
@@ -46,14 +57,23 @@ export class TrainingPlansComponent implements OnInit {
   }
 
   deleteTrainingPlan(index: number) {
-    // hier den modalService drauf werfen
-    if (
-      confirm('Bist du sicher, dass du diesen Trainingsplan löschen möchtest?')
-    ) {
+    // Store the index of the training plan to be deleted
+
+    this.deleteIndex = index;
+
+    this.modalService.open(
+      DeleteConfirmationComponent,
+      'Trainingsplan wirklich löschen?',
+      'Löschen'
+    );
+  }
+
+  private handleDelete(index: number) {
+    if (index >= 0) {
       this.httpClient
         .request<any>(HttpMethods.DELETE, `training/delete/${index}`)
         .subscribe({
-          next: (response: Response) => {
+          next: (response: any) => {
             this.trainingPlans.splice(index, 1);
           },
           error: (error: HttpErrorResponse) => {
---
+++ client/src/app/delete-confirmation/delete-confirmation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+import { AlertComponent } from '../components/alert/alert.component';
+
+@Component({
+  selector: 'app-delete-confirmation',
+  standalone: true,
+  imports: [],
+  templateUrl: './delete-confirmation.component.html',
+  styleUrl: './delete-confirmation.component.scss',
+})
+export class DeleteConfirmationComponent {}
