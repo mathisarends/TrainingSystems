1c1f7163ab517f979938cbaafeb9cd9653880181
mathisarends <ma197404@fh-muenster.de>
2024-07-20 12:28:57 +0200

    Adjust tooltip positioning

--- client/src/app/tooltip/tooltip.directive.ts
+++ client/src/app/tooltip/tooltip.directive.ts
@@ -18,24 +18,38 @@ export class TooltipDirective {
 
   @HostListener('mouseenter') onMouseEnter() {
     if (!this.tooltipElement) {
-      this.tooltipElement = this.renderer.createElement('span');
+      this.tooltipElement = this.renderer.createElement('div');
+      this.renderer.addClass(this.tooltipElement, 'tooltip-container');
+
+      const arrow = this.renderer.createElement('div');
+      this.renderer.addClass(arrow, 'tooltip-arrow');
+      this.renderer.appendChild(this.tooltipElement, arrow);
+
+      const text = this.renderer.createElement('span');
+      this.renderer.addClass(text, 'tooltip-text');
       this.renderer.appendChild(
-        this.tooltipElement,
+        text,
         this.renderer.createText(this.tooltipMessage)
       );
+      this.renderer.appendChild(this.tooltipElement, text);
+
       this.renderer.appendChild(document.body, this.tooltipElement);
-      this.renderer.addClass(this.tooltipElement, 'tooltip');
     }
     const rect = this.el.nativeElement.getBoundingClientRect();
     this.renderer.setStyle(
       this.tooltipElement,
       'top',
-      `${rect.top + window.scrollY - this.tooltipElement.offsetHeight - 10}px`
+      `${rect.top + window.scrollY - this.tooltipElement.offsetHeight - 7.5}px`
     );
     this.renderer.setStyle(
       this.tooltipElement,
       'left',
-      `${rect.left + window.scrollX}px`
+      `${
+        rect.left +
+        window.scrollX +
+        rect.width / 2 -
+        this.tooltipElement.offsetWidth / 2
+      }px`
     );
     this.renderer.setStyle(this.tooltipElement, 'visibility', 'visible');
     this.renderer.setStyle(this.tooltipElement, 'opacity', '1');
