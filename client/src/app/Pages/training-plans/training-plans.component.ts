2978665e036c090dd9ac06066bc88d3c08053ee0
mathisarends <ma197404@fh-muenster.de>
2024-07-28 07:55:05 +0200

    Use observables and loading spinners for loading trianing plans

--- client/src/app/Pages/training-plans/training-plans.component.ts
+++ client/src/app/Pages/training-plans/training-plans.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, DestroyRef, OnDestroy, OnInit } from '@angular/core';
 import { ModalService } from '../../../service/modal/modalService';
 import { CreateTrainingFormComponent } from '../create-training-form/create-training-form.component';
 import { HttpClientService } from '../../../service/http/http-client.service';
@@ -6,13 +6,14 @@ import { HttpMethods } from '../../types/httpMethods';
 import { AlertComponent } from '../../components/alert/alert.component';
 import { SpinnerComponent } from '../../components/spinner/spinner.component';
 import { TrainingPlanCardView } from '../../../../../shared/models/dtos/training/trainingDto.types.js';
-import { firstValueFrom, Subscription } from 'rxjs';
+import { firstValueFrom, BehaviorSubject } from 'rxjs';
 import { SearchService } from '../../../service/util/search.service';
 import { TrainingPlanService } from '../../../service/training/training-plan.service';
 import { CommonModule } from '@angular/common';
 import { TooltipDirective } from '../../../service/tooltip/tooltip.directive';
 import { TrainingPlanCardComponent } from '../../components/training-plan-card/training-plan-card.component';
 import { ModalSize } from '../../../service/modal/modalSize';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 /**
  * Component to manage and display training plans.
@@ -31,16 +32,19 @@ import { ModalSize } from '../../../service/modal/modalSize';
   styleUrls: ['./training-plans.component.scss'],
 })
 export class TrainingPlansComponent implements OnInit, OnDestroy {
-  protected allTrainingPlans!: TrainingPlanCardView[];
-  protected filteredTrainingPlans!: TrainingPlanCardView[];
-  protected isLoading: boolean = true;
-  private searchSubscription!: Subscription;
+  protected allTrainingPlans$ = new BehaviorSubject<
+    TrainingPlanCardView[] | null
+  >(null);
+  protected filteredTrainingPlans$ = new BehaviorSubject<
+    TrainingPlanCardView[] | null
+  >(null);
 
   constructor(
     private modalService: ModalService,
     private httpClient: HttpClientService,
     private searchService: SearchService,
-    private trainingPlanService: TrainingPlanService
+    private trainingPlanService: TrainingPlanService,
+    private destroyRef: DestroyRef
   ) {}
 
   /**
@@ -49,33 +53,40 @@ export class TrainingPlansComponent implements OnInit, OnDestroy {
    */
   async ngOnInit(): Promise<void> {
     await this.loadTrainingPlans();
-    this.subscribeToSearch();
-    this.subscribeToTrainingPlanChanges();
+
+    this.searchService.searchText$
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((searchText) => {
+        this.filterTrainingPlans(searchText);
+      });
+
+    this.trainingPlanService.trainingPlansChanged$
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe(() => {
+        this.loadTrainingPlans();
+      });
   }
 
   /**
    * Lifecycle hook that runs when the component is destroyed.
    * Unsubscribes from subscriptions to avoid memory leaks.
    */
-  ngOnDestroy(): void {
-    this.unsubscribeFromAll();
-  }
+  ngOnDestroy(): void {}
 
   /**
    * Loads training plans from the server.
    */
   private async loadTrainingPlans(): Promise<void> {
-    this.isLoading = true;
     try {
       const response: any = await firstValueFrom(
         this.httpClient.request<any>(HttpMethods.GET, 'training/plans')
       );
-      this.allTrainingPlans = response.trainingPlanDtos;
-      this.filteredTrainingPlans = this.allTrainingPlans;
+      this.allTrainingPlans$.next(response.trainingPlanDtos);
+      this.filteredTrainingPlans$.next(response.trainingPlanDtos);
     } catch (error) {
       console.error('Fehler beim Laden:', error);
-    } finally {
-      this.isLoading = false;
+      this.allTrainingPlans$.next([]);
+      this.filteredTrainingPlans$.next([]);
     }
   }
 
@@ -104,51 +115,20 @@ export class TrainingPlansComponent implements OnInit, OnDestroy {
    * @param searchText - The search input text.
    */
   private filterTrainingPlans(searchText: string): void {
-    this.filteredTrainingPlans = this.allTrainingPlans?.filter(
-      (trainingPlan) => {
-        return trainingPlan.title
-          .toLowerCase()
-          .includes(searchText.toLowerCase());
-      }
+    const allPlans = this.allTrainingPlans$.value || [];
+    this.filteredTrainingPlans$.next(
+      allPlans.filter((trainingPlan) =>
+        trainingPlan.title.toLowerCase().includes(searchText.toLowerCase())
+      )
     );
-  }
-
-  /**
-   * Subscribes to the search input changes.
-   */
-  private subscribeToSearch(): void {
-    this.searchSubscription = this.searchService.searchText$.subscribe(
-      (searchText) => {
-        this.filterTrainingPlans(searchText);
-      }
-    );
-  }
-
-  /**
-   * Subscribes to training plan changes.
-   */
-  private subscribeToTrainingPlanChanges(): void {
-    this.trainingPlanService.trainingPlansChanged$.subscribe(() => {
-      this.loadTrainingPlans();
-    });
-  }
-
-  /**
-   * Unsubscribes from all subscriptions to avoid memory leaks.
-   */
-  private unsubscribeFromAll(): void {
-    if (this.searchSubscription) {
-      this.searchSubscription.unsubscribe();
-    }
   }
 
   /**
    * Determines the column class based on the index.
-   * @param index - The index of the training plan.
    * @returns {string} - The column class.
    */
   getColumnClass(): string {
-    const totalItems = this.filteredTrainingPlans.length;
+    const totalItems = this.filteredTrainingPlans$.value?.length || 0;
     if (totalItems % 3 === 0) {
       return 'col-lg-4 col-md-6 col-sm-12';
     } else if (totalItems % 2 === 0) {
