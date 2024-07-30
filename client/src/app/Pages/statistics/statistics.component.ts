4d8dfdd73fd83bff462ceea5aa090ef225f125d4
mathisarends <ma197404@fh-muenster.de>
2024-07-30 18:11:01 +0200

    Implement first chart in statistic page

--- client/src/app/Pages/statistics/statistics.component.ts
+++ client/src/app/Pages/statistics/statistics.component.ts
@@ -1,12 +1,54 @@
 import { Component } from '@angular/core';
 
+import Chart from 'chart.js/auto';
+
 @Component({
   selector: 'app-statistics',
   standalone: true,
   imports: [],
   templateUrl: './statistics.component.html',
-  styleUrl: './statistics.component.scss'
+  styleUrl: './statistics.component.scss',
 })
 export class StatisticsComponent {
+  public chart: any;
+  public lineChart: any;
 
+  ngOnInit(): void {
+    this.createChart();
+  }
+
+  createChart() {
+    this.chart = new Chart('MyChart', {
+      type: 'bar', //this denotes tha type of chart
+
+      data: {
+        // values on X-Axis
+        labels: [
+          '2022-05-10',
+          '2022-05-11',
+          '2022-05-12',
+          '2022-05-13',
+          '2022-05-14',
+          '2022-05-15',
+          '2022-05-16',
+          '2022-05-17',
+        ],
+        datasets: [
+          {
+            label: 'Sales',
+            data: ['467', '576', '572', '79', '92', '574', '573', '576'],
+            backgroundColor: 'blue',
+          },
+          {
+            label: 'Profit',
+            data: ['542', '542', '536', '327', '17', '0.00', '538', '541'],
+            backgroundColor: 'limegreen',
+          },
+        ],
+      },
+      options: {
+        aspectRatio: 2.5,
+      },
+    });
+  }
 }
