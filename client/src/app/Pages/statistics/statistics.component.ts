c0797d22b4d87d82ef999efba39ee178e4a1f5a8
mathisarends <ma197404@fh-muenster.de>
2024-07-30 18:51:08 +0200

    Further Scratchs for stat page

--- client/src/app/Pages/statistics/statistics.component.ts
+++ client/src/app/Pages/statistics/statistics.component.ts
@@ -12,107 +12,12 @@ export class StatisticsComponent implements OnInit {
   public barChart: any;
   public lineChart: any;
   public fancyLineChart: any;
+  pieChart: any;
 
   ngOnInit(): void {
     this.createLineChart();
     this.createLineChartFancy();
-  }
-
-  /* Den hier für Tonnage verwenden */
-  createBarChart() {
-    if (this.barChart) {
-      this.barChart.destroy();
-    }
-
-    this.barChart = new Chart('MyBarChart', {
-      type: 'bar', // Typ des Diagramms
-      data: {
-        // Werte auf der X-Achse
-        labels: [
-          'Woche 1',
-          'Woche 2',
-          'Woche 3',
-          'Woche 4',
-          'Woche 5',
-          'Woche 6',
-        ],
-        datasets: [
-          {
-            label: 'Tonnage',
-            data: [300, 350, 320, 400, 370, 450], // Tonnage-Daten mit Schwankungen über 6 Wochen
-            backgroundColor: 'rgba(0, 0, 255, 0.5)', // Hintergrundfarbe mit Transparenz
-            borderColor: 'blue', // Randfarbe
-            borderWidth: 1, // Breite des Randes
-          },
-        ],
-      },
-      options: {
-        responsive: true, // Responsiveness aktivieren
-        maintainAspectRatio: false, // Seitenverhältnis nicht beibehalten
-        scales: {
-          x: {
-            title: {
-              display: true,
-              text: 'Wochen',
-            },
-          },
-          y: {
-            beginAtZero: true,
-            title: {
-              display: true,
-              text: 'Tonnage (kg)',
-            },
-          },
-        },
-        plugins: {
-          tooltip: {
-            callbacks: {
-              label: function (context) {
-                return 'Tonnage: ' + context.parsed.y + ' kg';
-              },
-            },
-          },
-        },
-      },
-    });
-  }
-
-  createLineChartFancy() {
-    if (this.fancyLineChart) {
-      this.fancyLineChart.destroy();
-    }
-
-    this.fancyLineChart = new Chart('MyFancyLineChart', {
-      type: 'line', //this denotes the type of chart
-      data: {
-        // values on X-Axis
-        labels: [
-          '2022-05-10',
-          '2022-05-11',
-          '2022-05-12',
-          '2022-05-13',
-          '2022-05-14',
-          '2022-05-15',
-          '2022-05-16',
-          '2022-05-17',
-        ],
-        datasets: [
-          {
-            label: 'Sales',
-            data: ['467', '576', '572', '79', '92', '574', '573', '576'],
-            backgroundColor: 'blue',
-          },
-          {
-            label: 'Profit',
-            data: ['542', '542', '536', '327', '17', '0.00', '538', '541'],
-            backgroundColor: 'limegreen',
-          },
-        ],
-      },
-      options: {
-        aspectRatio: 2.5,
-      },
-    });
+    this.createPieChart();
   }
 
   createLineChart() {
@@ -138,21 +43,21 @@ export class StatisticsComponent implements OnInit {
             data: [300, 350, 320, 400, 370, 450], // Tonnage-Daten für Squat über 6 Wochen
             borderColor: 'rgba(255, 99, 132, 1)', // Randfarbe
             backgroundColor: 'rgba(255, 99, 132, 0.2)', // Hintergrundfarbe mit Transparenz
-            fill: true, // Fläche unter der Linie füllen
+            fill: false, // Fläche unter der Linie füllen
           },
           {
             label: 'Bench',
             data: [200, 250, 220, 300, 270, 350], // Tonnage-Daten für Bench über 6 Wochen
             borderColor: 'rgba(54, 162, 235, 1)', // Randfarbe
             backgroundColor: 'rgba(54, 162, 235, 0.2)', // Hintergrundfarbe mit Transparenz
-            fill: true, // Fläche unter der Linie füllen
+            fill: false, // Fläche unter der Linie füllen
           },
           {
             label: 'Deadlift',
             data: [400, 450, 420, 500, 470, 550], // Tonnage-Daten für Deadlift über 6 Wochen
             borderColor: 'rgba(75, 192, 192, 1)', // Randfarbe
             backgroundColor: 'rgba(75, 192, 192, 0.2)', // Hintergrundfarbe mit Transparenz
-            fill: true, // Fläche unter der Linie füllen
+            fill: false, // Fläche unter der Linie füllen
           },
         ],
       },
@@ -160,12 +65,6 @@ export class StatisticsComponent implements OnInit {
         responsive: true, // Responsiveness aktivieren
         maintainAspectRatio: false, // Seitenverhältnis nicht beibehalten
         scales: {
-          x: {
-            title: {
-              display: true,
-              text: 'Wochen',
-            },
-          },
           y: {
             beginAtZero: true,
             title: {
@@ -179,6 +78,114 @@ export class StatisticsComponent implements OnInit {
             callbacks: {
               label: function (context) {
                 return context.dataset.label + ': ' + context.parsed.y + ' kg';
+              },
+            },
+          },
+        },
+      },
+    });
+  }
+
+  createLineChartFancy() {
+    if (this.fancyLineChart) {
+      this.fancyLineChart.destroy();
+    }
+
+    this.fancyLineChart = new Chart('MyFancyLineChart', {
+      type: 'line', // Typ des Diagramms
+      data: {
+        // Werte auf der X-Achse
+        labels: [
+          'Woche 1',
+          'Woche 2',
+          'Woche 3',
+          'Woche 4',
+          'Woche 5',
+          'Woche 6',
+        ],
+        datasets: [
+          {
+            label: 'Squat',
+            data: [300, 320, 330, 350, 370, 390], // Bestleistungen für Squat über 6 Wochen
+            borderColor: 'rgba(255, 99, 132, 1)', // Randfarbe
+            backgroundColor: 'rgba(255, 99, 132, 0.2)', // Hintergrundfarbe mit Transparenz
+            fill: true, // Fläche unter der Linie füllen
+          },
+          {
+            label: 'Bench',
+            data: [200, 220, 210, 230, 240, 250], // Bestleistungen für Bench über 6 Wochen
+            borderColor: 'rgba(54, 162, 235, 1)', // Randfarbe
+            backgroundColor: 'rgba(54, 162, 235, 0.2)', // Hintergrundfarbe mit Transparenz
+            fill: true, // Fläche unter der Linie füllen
+          },
+          {
+            label: 'Deadlift',
+            data: [400, 420, 410, 450, 460, 480], // Bestleistungen für Deadlift über 6 Wochen
+            borderColor: 'rgba(75, 192, 192, 1)', // Randfarbe
+            backgroundColor: 'rgba(75, 192, 192, 0.2)', // Hintergrundfarbe mit Transparenz
+            fill: true, // Fläche unter der Linie füllen
+          },
+          {
+            label: 'Overall Performance',
+            data: [900, 960, 950, 1030, 1070, 1120], // Gesamte Bestleistungen über 6 Wochen
+            borderColor: 'rgba(153, 102, 255, 1)', // Randfarbe
+            backgroundColor: 'rgba(153, 102, 255, 0.2)', // Hintergrundfarbe mit Transparenz
+            fill: true, // Fläche unter der Linie füllen
+          },
+        ],
+      },
+      options: {
+        responsive: true, // Responsiveness aktivieren
+        maintainAspectRatio: false, // Seitenverhältnis nicht beibehalten
+        scales: {
+          y: {
+            beginAtZero: true,
+            title: {
+              display: true,
+              text: 'Bestleistung in KG',
+            },
+          },
+        },
+        plugins: {
+          tooltip: {
+            callbacks: {
+              label: function (context) {
+                return context.dataset.label + ': ' + context.parsed.y + ' kg';
+              },
+            },
+          },
+        },
+      },
+    });
+  }
+
+  createPieChart() {
+    if (this.pieChart) {
+      this.pieChart.destroy();
+    }
+
+    this.pieChart = new Chart('MyPieChart', {
+      type: 'pie',
+      data: {
+        labels: ['Squat', 'Bench', 'Deadlift'],
+        datasets: [
+          {
+            data: [300, 200, 400], // Beispielwerte für die Gesamttonnage
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.5)',
+              'rgba(54, 162, 235, 0.5)',
+              'rgba(75, 192, 192, 0.5)',
+            ],
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        plugins: {
+          tooltip: {
+            callbacks: {
+              label: function (context) {
+                return context.label + ': ' + context.raw + ' kg';
               },
             },
           },
