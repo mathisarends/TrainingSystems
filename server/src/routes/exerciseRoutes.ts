f5eed083305db0ce1efd78ebcb7a51153c3cd34b
mathisarends <ma197404@fh-muenster.de>
2024-08-02 16:25:17 +0200

    Implement get all exercises route

--- server/src/routes/exerciseRoutes.ts
+++ server/src/routes/exerciseRoutes.ts
@@ -4,12 +4,37 @@ import { authService } from '../service/authService.js';
 import { MongoGenericDAO } from '../models/mongo-generic.dao.js';
 import { User } from '@shared/models/user.js';
 import { prepareExercisesData } from '../utils/exerciseUtils.js';
+import { ExerciseCategories } from '../utils/ExerciseCategores.js';
 
 const router = express.Router();
 
 router.get('/', authService.authenticationMiddleware, exerciseController.getExercises);
 router.patch('/', authService.authenticationMiddleware, exerciseController.updateExercises);
 router.post('/reset', authService.authenticationMiddleware, exerciseController.resetExercises);
+
+router.get('/categories', authService.authenticationMiddleware, async (req, res) => {
+  const userDAO: MongoGenericDAO<User> = req.app.locals.userDAO;
+  const userClaimsSet = res.locals.user;
+
+  try {
+    const user = await userDAO.findOne({ id: userClaimsSet.id });
+    if (!user) {
+      throw new Error('Benutzer nicht gefunden');
+    }
+
+    const categories = Object.values(ExerciseCategories).filter(
+      category => category !== ExerciseCategories.PLACEHOLDER
+    );
+
+    res.status(200).json(categories);
+  } catch (error) {
+    const errMessage = 'Fehler beim Abrufen der Kategorien ' + error;
+    res.status(500).json(errMessage);
+  }
+});
+
+// TODO: hier mit query parameter die category übergeben und danach entsprechend filtern dann
+router.get('/exercises', authService.authenticationMiddleware, async (req, res) => {});
 
 router.get('/training', authService.authenticationMiddleware, async (req, res) => {
   const userDAO: MongoGenericDAO<User> = req.app.locals.userDAO;
