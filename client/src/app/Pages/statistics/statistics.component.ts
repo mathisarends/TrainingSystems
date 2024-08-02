705b620ab466121b60866d7329976cfac24ee8f0
mathisarends <ma197404@fh-muenster.de>
2024-08-02 19:47:19 +0200

    Implement fetch of last visited categories

--- client/src/app/Pages/statistics/statistics.component.ts
+++ client/src/app/Pages/statistics/statistics.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import Chart from 'chart.js/auto';
 import { HttpClientService } from '../../../service/http/http-client.service';
 import { HttpMethods } from '../../types/httpMethods';
 import { SpinnerComponent } from '../../components/spinner/spinner.component';
@@ -29,8 +28,6 @@ import { GroupedBarChartComponent } from '../../grouped-bar-chart/grouped-bar-ch
   styleUrls: ['./statistics.component.scss'],
 })
 export class StatisticsComponent implements OnInit {
-  // TODO: statistic component refactor
-
   dataLoaded: boolean = false;
 
   selectedExercises!: string[];
@@ -38,6 +35,9 @@ export class StatisticsComponent implements OnInit {
 
   lineChartDatasets!: any[];
   lineChartLabels!: string[];
+
+  groupedBarChartDatasets!: number[];
+  groupedBarChartLabels!: string[];
 
   constructor(
     private router: Router,
@@ -47,27 +47,22 @@ export class StatisticsComponent implements OnInit {
 
   async ngOnInit(): Promise<void> {
     const id = this.router.url.split('/').pop();
-    await this.fetchTrainingStatistics(id);
+    if (id) {
+      await this.fetchInitialData(id);
+    }
   }
 
-  changeDisplayCategories(newExercises: string[]) {
-    const id = this.router.url.split('/').pop();
-    const exercises = newExercises.join(',');
+  async changeDisplayCategories(newExercises: string[]) {
+    const id = this.router.url.split('/').pop()!;
+    this.updateLastViewedCategories(id, newExercises);
 
-    this.httpService
-      .request<Partial<TrainingExerciseTonnageDto>>(
-        HttpMethods.GET,
-        `training/statistics/${id}?exercises=${exercises}`
-      )
-      .subscribe((response) => {
-        this.dataLoaded = true;
-        this.initializeCharts(response);
-      });
+    if (id) {
+      await this.fetchStatistics(id, newExercises);
+    }
   }
 
-  async fetchTrainingStatistics(id: string | undefined): Promise<void> {
+  private async fetchInitialData(id: string): Promise<void> {
     try {
-      // Führe beide Anfragen parallel aus und warte auf beide
       const [allExercisesResponse, selectedExercisesResponse] =
         await Promise.all([
           firstValueFrom(
@@ -87,33 +82,66 @@ export class StatisticsComponent implements OnInit {
       this.allExercises = allExercisesResponse;
       this.selectedExercises = selectedExercisesResponse;
 
-      this.httpService
-        .request<Partial<TrainingExerciseTonnageDto>>(
-          HttpMethods.GET,
-          `training/statistics/${id}?exercises=${this.selectedExercises}`
-        )
-        .subscribe((response) => {
-          this.dataLoaded = true;
-          this.initializeCharts(response);
-        });
+      await this.fetchStatistics(id, this.selectedExercises);
+    } catch (error) {
+      console.error('Error fetching initial data:', error);
+    }
+  }
+
+  private async fetchStatistics(
+    id: string,
+    exercises: string[]
+  ): Promise<void> {
+    try {
+      const exercisesQuery = exercises.join(',');
+
+      const [tonnageResponse, setsResponse] = await Promise.all([
+        firstValueFrom(
+          this.httpService.request<Partial<TrainingExerciseTonnageDto>>(
+            HttpMethods.GET,
+            `training/statistics/${id}?exercises=${exercisesQuery}`
+          )
+        ),
+        firstValueFrom(
+          this.httpService.request<any>(
+            HttpMethods.GET,
+            `training/statistics/${id}/sets?exercises=${exercisesQuery}`
+          )
+        ),
+      ]);
+
+      this.dataLoaded = true;
+      this.initializeCharts(tonnageResponse, setsResponse);
     } catch (error) {
       console.error('Error fetching training statistics:', error);
     }
   }
 
-  initializeCharts(data: Partial<TrainingExerciseTonnageDto>): void {
+  private initializeCharts(
+    tonnageResponse: Partial<TrainingExerciseTonnageDto>,
+    setsResponse: { [key: string]: number[] }
+  ): void {
     // Set data for the line chart
-    this.lineChartDatasets = Object.keys(data).map((categoryKey) => {
+    this.lineChartDatasets = Object.keys(tonnageResponse).map((categoryKey) => {
       const categoryData =
-        data[categoryKey as keyof TrainingExerciseTonnageDto];
-      return this.createDataset(categoryKey, categoryData || []);
+        tonnageResponse[categoryKey as keyof TrainingExerciseTonnageDto];
+      return this.createTonnageDataSet(categoryKey, categoryData || []);
     });
     this.lineChartLabels = this.generateWeekLabels(
       this.lineChartDatasets[0]?.data.length || 0
     );
+
+    // Set data for the grouped bar chart
+    this.groupedBarChartDatasets = Object.keys(setsResponse).map(
+      (categoryKey) => {
+        const setsData = setsResponse[categoryKey];
+        return this.createBarDataset(categoryKey, setsData);
+      }
+    );
+    this.groupedBarChartLabels = this.lineChartLabels; // Assuming both charts use the same labels (weeks)
   }
 
-  createDataset(category: string, data: Tonnage[]): any {
+  private createTonnageDataSet(category: string, data: Tonnage[]): any {
     const colors = this.chartColorService.getCategoryColor(category);
     return {
       label: this.formatCategoryLabel(category),
@@ -124,19 +152,34 @@ export class StatisticsComponent implements OnInit {
     };
   }
 
-  extractTonnageData(data: Tonnage[]): number[] {
+  private createBarDataset(category: string, data: number[]): any {
+    const colors = this.chartColorService.getCategoryColor(category);
+    return {
+      label: this.formatCategoryLabel(category),
+      data: data, // Use the raw numbers of sets per week
+      backgroundColor: colors.backgroundColor,
+    };
+  }
+
+  private extractTonnageData(data: Tonnage[]): number[] {
     return data.map((week) => week.tonnageInCategory);
   }
 
-  generateWeekLabels(length: number): string[] {
+  private generateWeekLabels(length: number): string[] {
     return Array.from({ length }, (_, index) => `Woche ${index + 1}`);
   }
 
-  formatCategoryLabel(category: string): string {
+  private formatCategoryLabel(category: string): string {
     return category.charAt(0).toUpperCase() + category.slice(1);
   }
 
-  calculateTotalTonnage(data: Tonnage[]): number {
-    return data.reduce((total, week) => total + week.tonnageInCategory, 0);
+  private updateLastViewedCategories(id: string, exericses: string[]) {
+    const exercisesQueryParam = exericses.join(',');
+
+    // Zuletzt besuchte Kategorien festse
+    this.httpService.request<any>(
+      HttpMethods.POST,
+      `training/statistics/${id}/viewedCategories?exercises=${exercisesQueryParam}`
+    );
   }
 }
