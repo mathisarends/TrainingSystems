e16fd62bdc07ba46c9af9718f456ccbf9d7c97c7
mathisarends <ma197404@fh-muenster.de>
2024-08-04 18:43:14 +0200

    Fetch prod url

--- client/src/config/environment.ts
+++ client/src/config/environment.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.ts
 export const environment = {
-  production: false,
+  production: true,
   apiUrl: 'http://localhost:3000',
   produUrl: 'https://trainingsystems.onrender.com',
 };
