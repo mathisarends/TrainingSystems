1b73da859c6bdfd4df8a94bf53caa941d2fd1632
mathisarends <ma197404@fh-muenster.de>
2024-08-19 21:13:16 +0200

    Implement save of last visited apge

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from './components/header/header.component';
 import { ToastComponent } from './components/toast/toast.component';
@@ -12,15 +12,30 @@ import { BrowserCheckService } from './browser-check.service';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'TrainingSystems';
 
   constructor(
     private serviceWorkerService: ServiceWorkerService,
     private browserCheckService: BrowserCheckService,
+    private router: Router,
   ) {
     if (this.browserCheckService.isBrowser()) {
       this.serviceWorkerService.registerServiceWorker();
+    }
+  }
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (this.browserCheckService.isBrowser() && event instanceof NavigationEnd) {
+        // Save the last route in localStorage
+        localStorage.setItem('lastRoute', event.urlAfterRedirects);
+      }
+    });
+
+    const lastRoute = localStorage.getItem('lastRoute');
+    if (lastRoute) {
+      this.router.navigateByUrl(lastRoute);
     }
   }
 }
