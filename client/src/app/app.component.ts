5cfc39f9d672ed929e5cd9bcb6725754fcaa4da1
mathisarends <ma197404@fh-muenster.de>
2024-08-20 19:46:46 +0200

    Simplified app component html

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from './components/header/header.component';
@@ -7,12 +7,13 @@ import { ServiceWorkerService } from './service-worker.service';
 import { BrowserCheckService } from './browser-check.service';
 import { MobileService } from '../service/util/mobile.service';
 import { RedirectService } from '../service/util/redirect.service';
+import { SpinnerComponent } from './components/loaders/spinner/spinner.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   providers: [RedirectService],
-  imports: [RouterOutlet, HeaderComponent, ToastComponent],
+  imports: [RouterOutlet, HeaderComponent, ToastComponent, SpinnerComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
@@ -30,11 +31,17 @@ export class AppComponent implements OnInit {
     }
   }
 
+  /**
+   * This method initializes the redirection service to start tracking the user's route navigation.
+   * It also checks if the app is being viewed on a mobile device and attempts to redirect the user
+   * to their last visited route if applicable.
+   */
   ngOnInit(): void {
-    this.redirectService.initialize();
-
-    if (this.mobileService.isMobileView()) {
-      this.redirectService.redirectToLastRoute();
+    if (!this.mobileService.isMobileView()) {
+      return;
     }
+
+    this.redirectService.initialize();
+    this.redirectService.redirectToLastRoute();
   }
 }
