2cbdce92e73aac0ca55563a0f7bb94f77704b50c
mathisarends <ma197404@fh-muenster.de>
2024-08-22 17:56:41 +0200

    Remove not working logic

--- client/src/directives/category-select.directive.ts
+++ client/src/directives/category-select.directive.ts
@@ -56,18 +56,13 @@ export class CategorySelectDirective {
     exerciseNameSelectors: NodeListOf<HTMLSelectElement>,
     tableRow: Element,
   ): void {
-    // Hide the category selector and reset exercise name selectors
     this.renderer.setStyle(categorySelector, 'opacity', '0');
     exerciseNameSelectors.forEach((selector) => {
       this.renderer.setStyle(selector, 'display', 'none');
       selector.disabled = false;
     });
 
-    // Reset the input values
     this.updateInputValues(tableRow, '- Bitte Auswählen -', this.exerciseDataService.getDefaultRepSchemeByCategory());
-
-    // Fill category gaps from subsequent rows
-    this.fillCategoryGaps(tableRow);
   }
 
   /**
@@ -79,10 +74,8 @@ export class CategorySelectDirective {
     tableRow: Element,
     category: string,
   ): void {
-    // Show the category selector
     this.renderer.setStyle(categorySelector, 'opacity', '1');
 
-    // Determine the index of the selected category and update selectors visibility
     const selectedCategoryIndex = this.getSelectedCategoryIndex(category);
     exerciseNameSelectors.forEach((selector, index) => {
       const isSelected = index === selectedCategoryIndex;
@@ -150,85 +143,6 @@ export class CategorySelectDirective {
       if (inputs.hasOwnProperty(key)) {
         const element = inputs[key as keyof ExerciseInputs];
         this.formService.addChange(element.name, element.value);
-      }
-    }
-  }
-
-  private fillCategoryGaps(tableRow: Element): void {
-    let nextRow = tableRow.nextElementSibling;
-
-    while (nextRow) {
-      const nextCategorySelector = nextRow.querySelector('.exercise-category-selector') as HTMLSelectElement;
-
-      if (
-        nextCategorySelector &&
-        nextCategorySelector.value !== '- Bitte Auswählen -' &&
-        nextCategorySelector.value !== ''
-      ) {
-        this.copyValuesToPreviousRow(tableRow, nextRow);
-        break;
-      }
-
-      nextRow = nextRow.nextElementSibling;
-    }
-  }
-
-  private copyValuesToPreviousRow(currentRow: Element, nextRow: Element): void {
-    const exerciseCategorySelector = currentRow.querySelector('.exercise-category-selector') as HTMLSelectElement;
-    const nextCategorySelector = nextRow.querySelector('.exercise-category-selector') as HTMLSelectElement;
-
-    if (exerciseCategorySelector && nextCategorySelector) {
-      const currentInputs = this.getInputElements(currentRow);
-      const nextInputs = this.getInputElements(nextRow);
-
-      exerciseCategorySelector.value = nextCategorySelector.value;
-      exerciseCategorySelector.dispatchEvent(new Event('change'));
-
-      this.copyInputValues(currentInputs, nextInputs);
-      this.updateFormServiceWithInputElements(exerciseCategorySelector, currentInputs);
-
-      this.resetNextRowValues(nextCategorySelector, nextInputs);
-    }
-  }
-
-  private getInputElements(row: Element) {
-    return {
-      exerciseNameSelect: row.querySelector('.exercise-name-selector') as HTMLSelectElement,
-      setsInput: row.querySelector('.sets') as HTMLInputElement,
-      repsInput: row.querySelector('.reps') as HTMLInputElement,
-      targetRPEInput: row.querySelector('.targetRPE') as HTMLInputElement,
-      weightInput: row.querySelector('.weight') as HTMLInputElement,
-      actualRPEInput: row.querySelector('.actualRPE') as HTMLInputElement,
-      estMaxInput: row.querySelector('.estMax') as HTMLInputElement,
-    };
-  }
-
-  private copyInputValues(currentInputs: any, nextInputs: any): void {
-    for (const key in currentInputs) {
-      if (currentInputs.hasOwnProperty(key) && nextInputs.hasOwnProperty(key)) {
-        currentInputs[key].value = nextInputs[key].value;
-      }
-    }
-  }
-
-  private updateFormServiceWithInputElements(categorySelector: HTMLSelectElement, inputs: any): void {
-    this.formService.addChange(categorySelector.name, categorySelector.value);
-
-    for (const key in inputs) {
-      if (inputs.hasOwnProperty(key)) {
-        this.formService.addChange(inputs[key].name, inputs[key].value);
-      }
-    }
-  }
-
-  private resetNextRowValues(categorySelector: HTMLSelectElement, inputs: any): void {
-    categorySelector.value = '- Bitte Auswählen -';
-    categorySelector.dispatchEvent(new Event('change'));
-
-    for (const key in inputs) {
-      if (inputs.hasOwnProperty(key)) {
-        inputs[key].value = '';
-        this.formService.addChange(inputs[key].name, inputs[key].value);
       }
     }
   }
