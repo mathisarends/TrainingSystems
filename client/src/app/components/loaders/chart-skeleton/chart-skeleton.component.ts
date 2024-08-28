e33c643ebcb05b21e68273e39a239408cd36e9fb
mathisarends <ma197404@fh-muenster.de>
2024-08-28 07:51:46 +0200

    Simplified training table loader

--- client/src/app/components/loaders/chart-skeleton/chart-skeleton.component.ts
+++ client/src/app/components/loaders/chart-skeleton/chart-skeleton.component.ts
@@ -1,6 +1,9 @@
 import { Component, input, OnInit, signal } from '@angular/core';
 import { SkeletonComponent } from '../../../skeleton/skeleton.component';
 
+/**
+ * Component that dynamically generates  a skeleton loading UI for a chart.
+ */
 @Component({
   selector: 'app-chart-skeleton',
   standalone: true,
@@ -9,8 +12,16 @@ import { SkeletonComponent } from '../../../skeleton/skeleton.component';
   styleUrl: './chart-skeleton.component.scss',
 })
 export class ChartSkeletonComponent implements OnInit {
+  /**
+   * Input property that specifies the number of rows for the skeleton component.
+   * Default value is set to 10.
+   */
   rows = input<number>(10);
 
+  /**
+   * Signal to store an array representing the rows for the skeleton.
+   * This array is used to dynamically render the skeleton rows in the template.
+   */
   rowsArray = signal<number[]>([]);
 
   ngOnInit(): void {
--- client/src/app/components/loaders/skeletons/skeleton-training-table/skeleton-training-table.component.ts
+++ client/src/app/components/loaders/skeletons/skeleton-training-table/skeleton-training-table.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { SkeletonComponent } from '../../../../skeleton/skeleton.component';
+import { SkeletonAnimationStyle } from '../../../../skeleton/skeleton-animation-style';
 
 @Component({
   selector: 'app-skeleton-training-table',
   standalone: true,
-  imports: [],
+  imports: [SkeletonComponent],
   templateUrl: './skeleton-training-table.component.html',
   styleUrl: './skeleton-training-table.component.scss',
 })
-export class SkeletonTrainingTableComponent {}
+export class SkeletonTrainingTableComponent {
+  protected readonly PULSE = SkeletonAnimationStyle.PULSE;
+}
