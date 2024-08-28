544fbe5c27eaad02d8d74fa52165c1aa25946908
mathisarends <ma197404@fh-muenster.de>
2024-08-28 06:53:37 +0200

    Simplified loading spinner in statistic page

--- client/src/app/components/loaders/chart-skeleton/chart-skeleton.component.ts
+++ client/src/app/components/loaders/chart-skeleton/chart-skeleton.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, input, OnInit, signal } from '@angular/core';
+import { SkeletonComponent } from '../../../skeleton/skeleton.component';
 
 @Component({
   selector: 'app-chart-skeleton',
   standalone: true,
-  imports: [],
+  imports: [SkeletonComponent],
   templateUrl: './chart-skeleton.component.html',
-  styleUrl: './chart-skeleton.component.scss'
+  styleUrl: './chart-skeleton.component.scss',
 })
-export class ChartSkeletonComponent {
+export class ChartSkeletonComponent implements OnInit {
+  rows = input<number>(10);
 
+  rowsArray = signal<number[]>([]);
+
+  ngOnInit(): void {
+    const rowsArray = Array.from({ length: this.rows() }, (_, i) => i);
+    this.rowsArray.set(rowsArray);
+  }
 }
