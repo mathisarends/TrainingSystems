ad6b745f4d0e0f92d7362b96107f8877fca3050b
mathisarends <ma197404@fh-muenster.de>
2024-08-28 07:14:39 +0200

    Use new skeleton component in training card loader

--- client/src/app/components/loaders/skeletons/skeleton-card/skeleton-card.component.ts
+++ client/src/app/components/loaders/skeletons/skeleton-card/skeleton-card.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { SkeletonComponent } from '../../../../skeleton/skeleton.component';
+import { SkeletonAnimationStyle } from '../../../../skeleton/skeleton-animation-style';
 
 @Component({
   selector: 'app-skeleton-card',
   standalone: true,
-  imports: [],
+  imports: [SkeletonComponent],
   templateUrl: './skeleton-card.component.html',
   styleUrl: './skeleton-card.component.scss',
 })
-export class SkeletonCardComponent {}
+export class SkeletonCardComponent {
+  protected readonly pulse = SkeletonAnimationStyle.PULSE;
+}
