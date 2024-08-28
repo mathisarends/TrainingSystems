ba7751d8e0b1548d2d85454c56fcba358d846e04
mathisarends <ma197404@fh-muenster.de>
2024-08-28 07:54:25 +0200

    Simplified skeleton template

--- client/src/app/components/loaders/skeletons/skeleton-training-table/skeleton-training-table.component.ts
+++ client/src/app/components/loaders/skeletons/skeleton-training-table/skeleton-training-table.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { SkeletonComponent } from '../../../../skeleton/skeleton.component';
 import { SkeletonAnimationStyle } from '../../../../skeleton/skeleton-animation-style';
 
@@ -10,5 +10,20 @@ import { SkeletonAnimationStyle } from '../../../../skeleton/skeleton-animation-
   styleUrl: './skeleton-training-table.component.scss',
 })
 export class SkeletonTrainingTableComponent {
+  /**
+   *  Pulse animation style of skeletons.
+   */
   protected readonly PULSE = SkeletonAnimationStyle.PULSE;
+
+  /**
+   * Signal representing the number of sections to generate.
+   * The default value is set to 10.
+   */
+  protected sections = signal<number[]>(Array.from({ length: 10 }, (_, i) => i + 1));
+
+  /**
+   * Signal representing the number of rows per section to generate.
+   * The default value is set to 9.
+   */
+  protected rowsPerSection = signal<number[]>(Array.from({ length: 9 }, (_, i) => i + 1));
 }
