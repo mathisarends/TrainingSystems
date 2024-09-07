a87967fd0bf13bdb9eda9f290bb4536f91c53f94
Mathis Arends <mathisarends27@gmail.com>
2024-09-07 21:18:44 +0200

    Implement ui and basic routes for password reset

--- client/src/app/Pages/auth/reset-password/reset-password.component.ts
+++ client/src/app/Pages/auth/reset-password/reset-password.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { BaisAuthComponent } from '../basic-auth.component';
+import { Router } from '@angular/router';
+import { HttpService } from '../../../../service/http/http-client.service';
+import { ToastService } from '../../../components/toast/toast.service';
+import { DOCUMENT } from '@angular/common';
 
+// TODO: implement this component with backend route
 @Component({
   selector: 'app-reset-password',
   standalone: true,
   imports: [],
   templateUrl: './reset-password.component.html',
-  styleUrl: './reset-password.component.scss',
+  styleUrls: ['../auth-shared.scss'],
 })
-export class ResetPasswordComponent {}
+export class ResetPasswordComponent extends BaisAuthComponent {
+  constructor(
+    router: Router,
+    httpClient: HttpService,
+    toastService: ToastService,
+    @Inject(DOCUMENT) document: Document,
+  ) {
+    super(router, httpClient, toastService, document);
+  }
+
+  onSubmit(event: Event) {
+    console.log('fetch things');
+    event.preventDefault();
+
+    this.toastService.success('E-Mail erfolgreich versendet');
+  }
+}
