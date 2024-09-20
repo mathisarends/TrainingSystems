4741aef4793f994bb4412f5cc9ba0b560ca47aed
Mathis Arends <mathisarends27@gmail.com>
2024-09-20 16:26:25 +0200

    Add clearance on click in search bar

--- client/src/app/shared/components/search-bar/search-bar.component.ts
+++ client/src/app/shared/components/search-bar/search-bar.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, output } from '@angular/core';
+import { Component, output, signal, WritableSignal } from '@angular/core';
 import { IconName } from '../../icon/icon-name';
 import { IconComponent } from '../../icon/icon.component';
 
@@ -13,13 +13,35 @@ import { IconComponent } from '../../icon/icon.component';
 export class SearchBarComponent {
   protected readonly IconName = IconName;
 
+  /**
+   * A WritableSignal that holds the current search query string.
+   */
+  protected searchQuery: WritableSignal<string> = signal('');
+
+  /**
+   * An output event that emits the current search query string whenever it changes.
+   */
   searchQueryChanged = output<string>();
 
   constructor() {}
 
-  // Called when the user types in the search input
+  /**
+   * Handles user input changes within the search bar.
+   *
+   * @param event The input change event object.
+   */
   onInputChange(event: Event) {
     const target = event.target as HTMLInputElement;
+
+    this.searchQuery.set(target.value);
     this.searchQueryChanged.emit(target.value);
+  }
+
+  /**
+   * Clears the current search query.
+   */
+  clearSearch() {
+    this.searchQuery.set('');
+    this.searchQueryChanged.emit('');
   }
 }
