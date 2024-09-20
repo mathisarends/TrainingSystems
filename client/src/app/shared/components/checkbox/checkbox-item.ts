d4fbc7d2146ee1229f4561585e38eed6cae6f9e2
Mathis Arends <mathisarends27@gmail.com>
2024-09-20 17:27:50 +0200

    Add documentation for checkbox component + interface

--- client/src/app/shared/components/checkbox/checkbox-item.ts
+++ client/src/app/shared/components/checkbox/checkbox-item.ts
@@ -1,4 +1,15 @@
+/**
+ * Interface representing a checkbox item.
+ * It holds the label for the checkbox and its checked state.
+ */
 export interface CheckboxItem {
+  /**
+   * The label for the checkbox.
+   */
   label: string;
+
+  /**
+   * Boolean value indicating whether the checkbox is checked or not.
+   */
   isChecked: boolean;
 }
--- client/src/app/shared/components/checkbox/checkbox.component.ts
+++ client/src/app/shared/components/checkbox/checkbox.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, HostListener, Injector, input, OnInit, output, signal } from '@angular/core';
+import { Component, effect, HostListener, input, output, signal } from '@angular/core';
 import { CheckboxItem } from './checkbox-item';
 
 @Component({
@@ -7,39 +7,62 @@ import { CheckboxItem } from './checkbox-item';
   templateUrl: './checkbox.component.html',
   styleUrl: './checkbox.component.scss',
 })
-export class CheckboxComponent implements OnInit {
+export class CheckboxComponent {
+  /**
+   * Input signal for the CheckboxItem object that contains the label and checked state.
+   */
   item = input<CheckboxItem>();
 
+  /**
+   * Holds the label of the checkbox.
+   */
   label = signal('');
 
-  checked = signal(false);
+  /**
+   * Signal that holds the checked state of the checkbox.
+   * Initialized based on the input CheckboxItem.
+   */
+  isChecked = signal(false);
 
+  /**
+   * Output event emitter that emits the updated CheckboxItem whenever the checkbox state changes.
+   */
   valueChanged = output<CheckboxItem>();
 
-  constructor(private injector: Injector) {}
-
-  ngOnInit(): void {
+  /**
+   * Initializes the component by creating an effect that synchronizes the `CheckboxItem`
+   * input with the internal label and checked signals.
+   */
+  constructor() {
     effect(
       () => {
         const item = this.item();
         if (item) {
           this.label.set(item.label);
-          this.checked.set(item.isChecked);
+          this.isChecked.set(item.isChecked);
         }
       },
-      { allowSignalWrites: true, injector: this.injector },
+      { allowSignalWrites: true },
     );
   }
 
+  /**
+   * Toggles the checkbox state and emits the updated CheckboxItem
+   * with the new label and checked state.
+   */
   toggleCheckbox() {
-    const newValue = !this.checked();
-    this.checked.set(newValue);
+    const newValue = !this.isChecked();
+    this.isChecked.set(newValue);
     this.valueChanged.emit({
       label: this.label(),
       isChecked: newValue,
     });
   }
 
+  /**
+   * When the host is clicked, it triggers the `toggleCheckbox()` function
+   * to toggle the checkbox state.
+   */
   @HostListener('click')
   onHostClick() {
     this.toggleCheckbox();
