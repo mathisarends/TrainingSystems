bbd01c762004c66d05403af4c575dfc17477e993
Mathis Arends <mathisarends27@gmail.com>
2024-09-21 17:57:23 +0200

    Refactor modal component using type guard

--- client/src/app/shared/components/modal/modal.component.ts
+++ client/src/app/shared/components/modal/modal.component.ts
@@ -13,6 +13,8 @@ import {
 import { ModalService } from '../../../core/services/modal/modalService';
 import { ModalSize } from '../../../core/services/modal/modalSize';
 import { ButtonComponent } from '../button/button.component';
+import { OnConfirm } from './on-confirm';
+import { OnToggleView } from './on-toggle-view';
 
 @Component({
   selector: 'app-modal',
@@ -47,31 +49,53 @@ export class ModalComponent implements AfterViewInit {
     this.loadChildComponent();
   }
 
+  /**
+   * Loads the child component dynamically into the modal content.
+   * The component's inputs will be set from the passed `childComponentData`.
+   */
   loadChildComponent() {
-    if (this.childComponentType) {
-      this.childComponentRef = createComponent(this.childComponentType, {
-        environmentInjector: this.environmentInjector,
-        elementInjector: this.modalContent.injector,
-      });
-      this.modalContent.insert(this.childComponentRef.hostView);
+    if (!this.childComponentType) {
+      return;
+    }
 
-      // Pass data to the child component
-      if (this.childComponentData) {
-        Object.keys(this.childComponentData).forEach((key) => {
-          this.childComponentRef.instance[key] = this.childComponentData[key];
-        });
-      }
+    this.childComponentRef = createComponent(this.childComponentType, {
+      environmentInjector: this.environmentInjector,
+      elementInjector: this.modalContent.injector,
+    });
+
+    this.modalContent.insert(this.childComponentRef.hostView);
+
+    if (this.childComponentData) {
+      this.setChildComponentInputs(this.childComponentData);
     }
   }
 
+  /**
+   * Sets inputs for the dynamically loaded child component.
+   * Only properties that exist in the child component will be set.
+   */
+  private setChildComponentInputs(data: any): void {
+    Object.keys(data).forEach((key) => {
+      this.childComponentRef.instance[key] = data[key];
+    });
+  }
+
+  /**
+   * Closes the modal and emits the `cancelled` event.
+   */
   close() {
     this.cancelled.emit();
     this.modalService.close();
   }
 
+  /**
+   * Confirms the modal's action and emits the `confirmed` event.
+   * Calls `onConfirm` if the child component implements `ConfirmableComponent`.
+   */
   confirm() {
-    if (this.childComponentRef.instance.onConfirm) {
-      this.childComponentRef.instance.onConfirm();
+    const componentInstance = this.childComponentRef.instance;
+    if (this.implementsOnConfirm(componentInstance)) {
+      componentInstance.onConfirm();
     }
 
     if (!this.confirmationRequired) {
@@ -81,9 +105,27 @@ export class ModalComponent implements AfterViewInit {
     this.confirmed.emit();
   }
 
+  /**
+   * Toggles the modal view if the child component defines an `onToggleView` method.
+   */
   toggleModalView() {
-    if (this.childComponentRef.instance.onToggleView) {
-      this.childComponentRef.instance.onToggleView();
+    const componentInstance = this.childComponentRef.instance;
+    if (this.implementsOnToggleView(componentInstance)) {
+      componentInstance.onToggleView();
     }
+  }
+
+  /**
+   * Type guard to check if a component is ConfirmableComponent
+   */
+  private implementsOnConfirm(component: any): component is OnConfirm {
+    return (component as OnConfirm).onConfirm !== undefined;
+  }
+
+  /**
+   * Type guard to check if a component is ToggleableViewComponent
+   */
+  private implementsOnToggleView(component: any): component is OnToggleView {
+    return (component as OnToggleView).onToggleView !== undefined;
   }
 }
