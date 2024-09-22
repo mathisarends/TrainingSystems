5679fe0b16f1d77515651fe457d81ffca8b4642a
Mathis Arends <mathisarends27@gmail.com>
2024-09-22 09:54:54 +0200

    Add documentation for new logic

--- client/src/app/features/header/header.service.ts
+++ client/src/app/features/header/header.service.ts
@@ -2,17 +2,52 @@ import { Injectable, signal } from '@angular/core';
 import { IconName } from '../../shared/icon/icon-name';
 import { HeadlineInfo } from './headline-info';
 
+/**
+ * Service to manage and update the header information for the application.
+ * This includes managing title, subtitle, icons, options, button actions, and loading states.
+ */
 @Injectable({
   providedIn: 'root',
 })
 export class HeaderService {
+  /**
+   * Stores the current title of the header.
+   */
   title = signal<string>('Training');
+
+  /**
+   * Holds the current subtitle of the header.
+   */
   subTitle = signal<string>('TYR');
+
+  /**
+   * Holds the icon name for the header, if any.
+   * Can be undefined if no icon is set.
+   */
   iconName = signal<IconName | undefined>(undefined);
+
+  /**
+   * Holds the list of options for additional actions in the header.
+   */
   options = signal<string[]>([]);
+
+  /**
+   * Signal that stores the callback function for handling button clicks in the header.
+   * Can be undefined if no callback is provided.
+   */
   onButtonClickCallback = signal<(() => void) | undefined>(undefined);
+
+  /**
+   * Signal that represents whether the header is in a loading state.
+   */
   loading = signal<boolean>(false);
 
+  /**
+   * Updates the header information such as the title, subtitle, icon, options, and button callback.
+   * Also stops the loading state by setting the `loading` signal to `false`.
+   *
+   * @param headlineInfo - The object containing the updated headline information.
+   */
   setHeadlineInfo(headlineInfo: HeadlineInfo) {
     this.title.set(headlineInfo.title);
     this.subTitle.set(headlineInfo.subTitle ?? 'TYR');
@@ -22,14 +57,10 @@ export class HeaderService {
     this.loading.set(false);
   }
 
+  /**
+   * Sets the header to a loading state. This can be called to indicate that the header is processing an action.
+   */
   setLoading() {
     this.loading.set(true);
-  }
-
-  handleButtonClick() {
-    const callback = this.onButtonClickCallback();
-    if (callback) {
-      callback();
-    }
   }
 }
--- client/src/app/features/header/headline-info.ts
+++ client/src/app/features/header/headline-info.ts
@@ -1,9 +1,37 @@
 import { IconName } from '../../shared/icon/icon-name';
 
+/**
+ * Interface that represents the structure of headline information for the header component.
+ * This includes the title, optional subtitle, optional icon, button click callback, and action options.
+ */
 export interface HeadlineInfo {
+  /**
+   * The main title of the header.
+   * This is a required property and will be displayed as the main heading in the header.
+   */
   title: string;
+
+  /**
+   * The optional subtitle of the header.
+   * Will be replaced with 'TYR' if not set.
+   */
   subTitle?: string;
+
+  /**
+   * The optional icon name to display in the header.
+   * If provided, the icon will be displayed alongside the title or in place of other elements.
+   */
   iconName?: IconName;
+
+  /**
+   * An optional callback function that is triggered when the associated header button is clicked.
+   * This allows for custom actions to be performed when the user interacts with the header.
+   */
   onButtonClickCallback?: () => void;
+
+  /**
+   * An optional list of action options for the header.
+   * This can be used to provide additional actions that the user can select.
+   */
   options?: string[];
 }
