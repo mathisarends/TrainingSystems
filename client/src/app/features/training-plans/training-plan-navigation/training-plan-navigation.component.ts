cc590ae655567cf375dfad7b72c8449417f20524
Mathis Arends <mathisarends27@gmail.com>
2024-09-26 13:20:36 +0200

    Add nice subtle scroll bar

---
+++ client/src/app/features/training-plans/training-plan-navigation/training-plan-navigation.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-training-plan-navigation',
+  standalone: true,
+  templateUrl: './training-plan-navigation.component.html',
+  styleUrls: ['./training-plan-navigation.component.scss'],
+})
+export class TrainingPlanNavigationComponent {}
