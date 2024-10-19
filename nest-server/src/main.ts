b83291262a2bd3c88b52bca15028982c07cc602d
Mathis Arends <mathisarends27@gmail.com>
2024-10-19 08:40:23 +0200

    Configured cors options

--- nest-server/src/main.ts
+++ nest-server/src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
+import * as cors from 'cors';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.use(cookieParser());
+  app.use(
+    cors({
+      origin: [
+        'http://localhost:3000',
+        'https://trainingsystemsre.onrender.com',
+      ],
+      methods: 'GET,POST,PUT,DELETE',
+      credentials: true,
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
