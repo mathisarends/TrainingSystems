7ace510176bd07c79409d8b9867cf807be5a28fc
Mathis Arends <mathisarends27@gmail.com>
2024-10-24 06:13:52 +0200

    Implement api for activity calendar

--- nest-server/src/activity-calendar/activity-calendar.controller.ts
+++ nest-server/src/activity-calendar/activity-calendar.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { GetUser } from 'src/decorators/user.decorator';
+import { TrainingService } from 'src/training/training.service';
+import { ActivityCalendarService } from './activity-calendar.service';
 
 @Controller('activity-calendar')
-export class ActivityCalendarController {}
+export class ActivityCalendarController {
+  constructor(
+    private readonly actvityCalendarService: ActivityCalendarService,
+    private readonly trainingService: TrainingService,
+  ) {}
+
+  @Get()
+  async getActivityCalendar(@GetUser() userId: string) {
+    const trainingPlans =
+      await this.trainingService.getTrainingPlansByUser(userId);
+    return this.actvityCalendarService.geetActivityCalendar(trainingPlans);
+  }
+}
--- nest-server/src/activity-calendar/activity-calendar.module.ts
+++ nest-server/src/activity-calendar/activity-calendar.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TrainingModule } from 'src/training/training.module';
+import { TrainingService } from 'src/training/training.service';
 import { ActivityCalendarController } from './activity-calendar.controller';
 import { ActivityCalendarService } from './activity-calendar.service';
 
 @Module({
+  imports: [TrainingModule],
   controllers: [ActivityCalendarController],
-  providers: [ActivityCalendarService]
+  providers: [ActivityCalendarService, TrainingService],
 })
 export class ActivityCalendarModule {}
