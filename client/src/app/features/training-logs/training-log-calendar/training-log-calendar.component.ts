380e5ec30cb3f04cb81743f672cff1e6cd2aad57
Mathis Arends <mathisarends27@gmail.com>
2024-11-03 09:58:24 +0100

    Add overlapping calendar dates and use signals

--- client/src/app/features/training-logs/training-log-calendar/training-log-calendar.component.ts
+++ client/src/app/features/training-logs/training-log-calendar/training-log-calendar.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
 
 @Component({
   selector: 'app-training-log-calendar',
@@ -9,12 +9,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./training-log-calendar.component.scss']
 })
 export class TrainingLogCalendarComponent {
-  currentMonth: string = 'November 2024';
-  daysInMonth: number[] = Array.from({ length: 30 }, (_, i) => i + 1);
+    currentDay = signal(new Date().getDate());
+    currentMonth = signal(new Date().getMonth());
+    currentYear = signal(new Date().getFullYear());
+  
+    daysInMonth: WritableSignal<number[]> = signal([]);
+    daysFromPreviousMonth: WritableSignal<number[]> = signal([]);
+    daysFromNextMonth: WritableSignal<number[]> = signal([]);
+  
+    constructor() {
+      this.generateCalendar();
+    }
+  
+    generateCalendar() {
+      const daysInCurrentMonth = new Date(this.currentYear(), this.currentMonth() + 1, 0).getDate();
+
+      const firstDayOfMonth = new Date(this.currentYear(), this.currentMonth(), 1).getDay();
+      const daysInPreviousMonth = new Date(this.currentYear(), this.currentMonth(), 0).getDate();
+      
+      const startIndex = (firstDayOfMonth + 6) % 7; // Um sicherzustellen, dass Montag der erste Tag ist
+  
+      const daysFromPreviousMonth =  Array.from({ length: startIndex }, (_, i) => daysInPreviousMonth - startIndex + i + 1);
+      this.daysFromPreviousMonth.set(daysFromPreviousMonth);
+
+      const daysInMonth = Array.from({ length: daysInCurrentMonth }, (_, i) => i + 1);
+      this.daysInMonth.set(daysInMonth);
+
+      // Berechnung der Tage des nächsten Monats, um den Kalender zu vervollständigen
+      const totalDays = this.daysFromPreviousMonth.length + this.daysInMonth.length;
+      const remainingDays = 7 - (totalDays % 7);        
+
+      const daysFromNextMonth = remainingDays < 7 ? Array.from({ length: remainingDays }, (_, i) => i + 1) : [];;
+      this.daysFromNextMonth.set(daysFromNextMonth);
+    }
     
-  currentDay: number = new Date().getDate(); 
 
-
-  constructor() {
-  }
 }
