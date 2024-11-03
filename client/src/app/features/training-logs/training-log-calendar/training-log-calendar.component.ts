bc375bced6fad72562f1a9ff63ce5a4941b3527f
Mathis Arends <mathisarends27@gmail.com>
2024-11-03 10:11:14 +0100

    Configure prettier with eslint

--- client/src/app/features/training-logs/training-log-calendar/training-log-calendar.component.ts
+++ client/src/app/features/training-logs/training-log-calendar/training-log-calendar.component.ts
@@ -1,14 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal, WritableSignal } from '@angular/core';
+import { IconName } from '../../../shared/icon/icon-name';
+import { IconComponent } from '../../../shared/icon/icon.component';
 
 @Component({
   selector: 'app-training-log-calendar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, IconComponent],
   templateUrl: './training-log-calendar.component.html',
   styleUrls: ['./training-log-calendar.component.scss']
 })
 export class TrainingLogCalendarComponent {
+    protected readonly IconName = IconName;
+
     currentDay = signal(new Date().getDate());
     currentMonth = signal(new Date().getMonth());
     currentYear = signal(new Date().getFullYear());
@@ -21,13 +25,13 @@ export class TrainingLogCalendarComponent {
       this.generateCalendar();
     }
   
-    generateCalendar() {
+    private generateCalendar() {
       const daysInCurrentMonth = new Date(this.currentYear(), this.currentMonth() + 1, 0).getDate();
 
       const firstDayOfMonth = new Date(this.currentYear(), this.currentMonth(), 1).getDay();
       const daysInPreviousMonth = new Date(this.currentYear(), this.currentMonth(), 0).getDate();
       
-      const startIndex = (firstDayOfMonth + 6) % 7; // Um sicherzustellen, dass Montag der erste Tag ist
+      const startIndex = (firstDayOfMonth + 6) % 7; 
   
       const daysFromPreviousMonth =  Array.from({ length: startIndex }, (_, i) => daysInPreviousMonth - startIndex + i + 1);
       this.daysFromPreviousMonth.set(daysFromPreviousMonth);
@@ -35,11 +39,11 @@ export class TrainingLogCalendarComponent {
       const daysInMonth = Array.from({ length: daysInCurrentMonth }, (_, i) => i + 1);
       this.daysInMonth.set(daysInMonth);
 
-      // Berechnung der Tage des nächsten Monats, um den Kalender zu vervollständigen
-      const totalDays = this.daysFromPreviousMonth.length + this.daysInMonth.length;
+      const totalDays = this.daysFromPreviousMonth().length + this.daysInMonth().length;
       const remainingDays = 7 - (totalDays % 7);        
 
       const daysFromNextMonth = remainingDays < 7 ? Array.from({ length: remainingDays }, (_, i) => i + 1) : [];;
+      console.log("🚀 ~ TrainingLogCalendarComponent ~ generateCalendar ~ daysFromNextMonth:", daysFromNextMonth)
       this.daysFromNextMonth.set(daysFromNextMonth);
     }
     
