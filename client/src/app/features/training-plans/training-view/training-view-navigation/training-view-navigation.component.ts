519c832dcd598681709e8c04e3cfb4d984135d9a
Mathis Arends <mathisarends27@gmail.com>
2024-12-03 07:56:23 +0100

    Use new navigation arrows for training navigation

--- client/src/app/features/training-plans/training-view/training-view-navigation/training-view-navigation.component.ts
+++ client/src/app/features/training-plans/training-view/training-view-navigation/training-view-navigation.component.ts
@@ -1,6 +1,6 @@
 import { Component, input } from '@angular/core';
+import { NavigationArrowsComponent } from '../../../../shared/components/navigation-arrows/navigation-arrows.component';
 import { PaginationComponent } from '../../../../shared/components/pagination/pagination.component';
-import { TooltipDirective } from '../../../../shared/directives/tooltip.directive';
 import { IconName } from '../../../../shared/icon/icon-name';
 import { IconComponent } from '../../../../shared/icon/icon.component';
 import { NavigationDirection } from '../models/navigation-direction.enum';
@@ -10,15 +10,19 @@ import { TrainingViewNavigationService } from '../training-view-navigation.servi
 @Component({
   selector: 'app-training-view-navigation',
   standalone: true,
-  imports: [IconComponent, PaginationComponent, TooltipDirective],
+  imports: [IconComponent, PaginationComponent, NavigationArrowsComponent],
   templateUrl: './training-view-navigation.component.html',
   styleUrls: ['./training-view-navigation.component.scss'],
 })
 export class TrainingViewNavigationComponent {
-    protected readonly IconName = IconName;
-    protected readonly NavigationDirection = NavigationDirection;
+  protected readonly IconName = IconName;
+  protected readonly NavigationDirection = NavigationDirection;
 
-    trainingDayIndex = input.required<number>();
+  trainingDayIndex = input.required<number>();
+  trainingWeekIndex = input.required<number>();
 
-    constructor(protected trainingDataService: TrainingPlanDataService, protected navigationService: TrainingViewNavigationService) {}
+  constructor(
+    protected trainingDataService: TrainingPlanDataService,
+    protected navigationService: TrainingViewNavigationService,
+  ) {}
 }
